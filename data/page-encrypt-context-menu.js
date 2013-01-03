02e48a990dbdbf896b0229e6538806f36108aaba
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2013-01-03 19:08:49 +0100

    Encryption improvements.
    - Properly walk up the DOM to find:
      * contentEditable elements,
      * textarea's,
      * iframes with designMode == "on".
    - This requires the latest git version of the SDK to work.
    - We're being hit by bug 826080

--- data/page-encrypt-context-menu.js
+++ data/page-encrypt-context-menu.js
@@ -4,27 +4,49 @@
 
 "use strict";
 
+function findEditable(aNode) {
+  if (aNode.tagName == "TEXTAREA")
+    return aNode;
+  else if (aNode.tagName == "IFRAME" &&
+      node.contentDocument &&
+      node.contentDocument.designMode == "on")
+    return aNode;
+  else if (aNode.contentEditable)
+    return aNode;
+  else if (aNode.parentNode)
+    return findIframe(aNode.parentNode);
+  else
+    return null;
+}
+
+function getText(aNode) {
+  switch (aNode.tagName) {
+    case "TEXTAREA":
+      return aNode.value;
+    case "IFRAME":
+      return aNode.contentDocument.body.textContent;
+    default:
+      return aNode.textContent;
+  }
+}
+
 self.on("click", function (node, data) {
   console.log("Menu item clicked");
+
+  let editableNode = findEditable(node);
+
   let className = "gpgfox-"+Math.round(Math.random()*1000);
-  node.classList.add(className);
+  editableNode.classList.add(className);
   console.log("Requesting encryption");
   self.postMessage({
     className: className,
-    text: node.value,
+    text: getText(editableNode),
   });
 });
 
 self.on("context", function (node) {
-  let tagName = node.tagName && node.tagName.toLowerCase();
-  console.log("context", tagName, node.contentDocument,
+  console.log("context", node.tagName, node.contentDocument,
     node.contentDocument && node.contentDocument.designMode);
-  let r =
-    (tagName == "textarea") ||
-    (tagName == "iframe"
-      && node.contentDocument
-      && node.contentDocument.designMode == "on") ||
-    (tagName == "iframe" && node.contentEditable)
-  ;
-  return r;
-});
+
+  return (findEditable(node) != null);
+ });
